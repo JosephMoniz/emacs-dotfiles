92b21c4c6abb2979e392215a7ad74290f36aead6
Joseph Moniz <joseph.moniz@gmail.com>
2013-05-20 16:55:03 -0700

    added window resizing keys

--- components/custom-keys.el
+++ components/custom-keys.el
@@ -1,4 +1,10 @@
-(global-set-key "\C-l" 'goto-line)
-(global-set-key "\C-f" 'forward-word)
-(global-set-key "\C-b" 'backward-word)
-(global-set-key "\C-g" 'magit-status)
+(global-set-key (kbd "C-l") 'goto-line)
+(global-set-key (kbd "C-f") 'forward-word)
+(global-set-key (kbd "C-b") 'backward-word)
+(global-set-key (kbd "C-g") 'magit-status)
+
+;; Playing with stuff
+(global-set-key (kbd "C-,") 'shrink-window-horizontally)
+(global-set-key (kbd "C-.") 'enlarge-window-horizontally)
+(global-set-key (kbd "C-;") 'shrink-window)
+(global-set-key (kbd "C-'") 'enlarge-window)
