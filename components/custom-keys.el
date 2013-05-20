3b4564a7f1f5f4eb2671b1d447e89557abe95645
Joseph Moniz <joseph.moniz@gmail.com>
2013-05-20 17:56:49 -0600

    added som docs

--- components/custom-keys.el
+++ components/custom-keys.el
@@ -1,9 +1,14 @@
+;; Go to line sugar
 (global-set-key (kbd "C-l") 'goto-line)
+
+;; Word skipping keys
 (global-set-key (kbd "C-f") 'forward-word)
 (global-set-key (kbd "C-b") 'backward-word)
+
+;; Shortcut to magit
 (global-set-key (kbd "C-g") 'magit-status)
 
-;; Playing with stuff
+;; Window resizing keys
 (global-set-key (kbd "C-,") 'shrink-window-horizontally)
 (global-set-key (kbd "C-.") 'enlarge-window-horizontally)
 (global-set-key (kbd "C-;") 'shrink-window)
